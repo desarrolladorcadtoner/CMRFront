b7c75d4383fc9142427a7e4d8c510e267f0d567d
desarrolladorcadtoner <asistente_desarrollador1@cadtoner.com.mx>
2025-04-30 09:00:06 -0600

    update links

--- crm-dist/src/pages/Home/index.tsx
+++ crm-dist/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import SidebarMenu from "@/components/sidebarMenu";
+import { Accordion, AccordionTab } from 'primereact/accordion';
 
 interface Product {
   id: string;
@@ -19,6 +20,7 @@ interface Product {
 export default function Login() {
   const [products, setProducts] = useState<Product[]>([]);
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
+  const [items] = useState<string[]>(Array.from({ length: 100 }).map((_, i) => `Item #${i}`));
 
   const handleSidebarToggle = (isExpanded: boolean) => {
     setIsSidebarExpanded(isExpanded);
@@ -182,9 +184,35 @@ export default function Login() {
             </div>
 
             {/* Módulo B */}
-            <div className="ModuloB h-48  bg-white shadow-md rounded-lg p-4">
+            <div className="ModuloB h-78  bg-white shadow-md rounded-lg p-4">
               <h2 className="text-xl font-semibold mb-4">Estadísticas</h2>
               <p>Aquí puedes agregar gráficos o estadísticas relacionadas con los productos.</p>
+              <Accordion activeIndex={0}>
+                <AccordionTab header="Header I">
+                  <p className="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </AccordionTab>
+                <AccordionTab header="Header II">
+                  <p className="m-0">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                    quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+                    sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                    Consectetur, adipisci velit, sed quia non numquam eius modi.
+                  </p>
+                </AccordionTab>
+                <AccordionTab header="Header III">
+                  <p className="m-0">
+                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                    quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
+                    mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+                    Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                  </p>
+                </AccordionTab>
+              </Accordion>
             </div>
           </div>
 
